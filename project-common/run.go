bfd8acecddf41a66f74cba723ca5a501e99bb921
omen <571462373@qq.com>
2025-04-11 21:25:32 +0800

    add project-common module and setup server with graceful shutdown

---
+++ project-common/run.go
@@ -0,0 +1,45 @@
+package common
+
+import (
+	"context"
+	"errors"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func Run(r *gin.Engine, srvName string, addr string) {
+	srv := &http.Server{
+		Addr:    addr,
+		Handler: r,
+	}
+	//guaranteed elegant start and stop below
+	go func() {
+		log.Printf("%s running in %s \n", srvName, srv.Addr)
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalln(err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	//SIGINT when user input Ctrl+C shutdown the program
+	//SIGTERM shutdown the program when the system send a signal
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	log.Printf("Shutting Down project %s ...\n", srvName)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("%s Shutdown, cause by : %v", srvName, err)
+	}
+	select {
+	case <-ctx.Done():
+		log.Println("client timeout, force shutdown")
+	}
+	log.Printf("%s stop success...", srvName)
+}
---
+++ project-user/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	srv "test.com/project-common"
+)
+
+func main() {
+	r := gin.Default()
+	srv.Run(r, "project-user", ":80")
+
+}
